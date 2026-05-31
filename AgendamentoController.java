0175a744a36f7bde835bd26139b2a145fa34de7a
Laís Lívia <lais.lsmarinho@senacsp.edu.br>
2026-05-31 14:52:40 -0300

    delet

--- AgendamentoController.java
+++
@@ -1,7 +0,0 @@
-package com.example.petshopapi.exception;
-
-public class RecursoNaoEncontradoException extends RuntimeException {
-    public RecursoNaoEncontradoException(Long id) { super("Recurso com ID " + id + " não encontrado."); }
-    public RecursoNaoEncontradoException(String recurso, Long id) { super(recurso + " com ID " + id + " não encontrado."); }
-    public RecursoNaoEncontradoException(String mensagem) { super(mensagem); }
-}
--- UnidadeController.java
+++
@@ -1,5 +0,0 @@
-package com.example.petshopapi.exception;
-
-public class ConflictException extends RuntimeException {
-    public ConflictException(String mensagem) { super(mensagem); }
-}
