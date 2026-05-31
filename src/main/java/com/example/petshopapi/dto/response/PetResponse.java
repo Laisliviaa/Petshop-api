fdee8746b8f998f9c5ef1e0a1161494b0cca32dc
Laís Lívia <lais.lsmarinho@senacsp.edu.br>
2026-05-31 19:24:10 -0300

    Update PetResponse.java

--- src/main/java/com/example/petshopapi/dto/response/PetResponse.java
+++ src/main/java/com/example/petshopapi/dto/response/PetResponse.java
@@ -3,13 +3,17 @@ package com.example.petshopapi.dto.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.hateoas.RepresentationModel;
 
-@Getter @AllArgsConstructor
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
+@Schema(description = "Dados de resposta de um pet")
 public class PetResponse extends RepresentationModel<PetResponse> {
-    @Schema(example = "1") private Long id;
-    @Schema(example = "Rex") private String nome;
-    @Schema(example = "Cachorro") private String especie;
-    @Schema(example = "Carlos Lima") private String nomeCliente;
-    @Schema(example = "3") private int totalServicos;
+    @Schema(example = "1")            private Long   id;
+    @Schema(example = "Rex")          private String nome;
+    @Schema(example = "Cachorro")     private String especie;
+    @Schema(example = "1")            private Long   clienteId;
+    @Schema(example = "Carlos Lima")  private String nomeCliente;
+    @Schema(example = "3")            private int    totalServicos;
 }
