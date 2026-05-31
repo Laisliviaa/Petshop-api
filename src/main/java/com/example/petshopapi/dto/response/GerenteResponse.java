f98cecc161d748abbf629395b150261d057b16fb
Laís Lívia <lais.lsmarinho@senacsp.edu.br>
2026-05-31 19:23:32 -0300

    Update GerenteResponse.java

--- src/main/java/com/example/petshopapi/dto/response/GerenteResponse.java
+++ src/main/java/com/example/petshopapi/dto/response/GerenteResponse.java
@@ -3,11 +3,15 @@ package com.example.petshopapi.dto.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.hateoas.RepresentationModel;
 
-@Getter @AllArgsConstructor
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
+@Schema(description = "Dados de resposta de um gerente")
 public class GerenteResponse extends RepresentationModel<GerenteResponse> {
-    @Schema(example = "1") private Long id;
-    @Schema(example = "Maria Souza") private String nome;
+    @Schema(example = "1")              private Long   id;
+    @Schema(example = "Maria Souza")    private String nome;
+    @Schema(example = "1")             private Long   unidadeId;
     @Schema(example = "Unidade Centro") private String nomeUnidade;
 }
